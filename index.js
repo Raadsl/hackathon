45bb9aa657603fc2f7bec88d36051e1160c79713
Jorik <83394320+Raadsl@users.noreply.github.com>
2022-10-04 16:38:43 +0200

    Update index.js
    added function str()

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ module.exports = function log(string) { //shorter console.log???
   console.log("string") 
 };
 
-module.exports = function log(string) { //shorter console.log???
-  console.log("string") 
+module.exports = function str(string) { //converts 
+  return string.toString()
 };
