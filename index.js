34f4598fd70f18a234a1dca9c6a9777d15c89559
Jorik <83394320+Raadsl@users.noreply.github.com>
2022-10-04 16:47:03 +0200

    Update index.js
    added int()

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ module.exports = function log(string) { //shorter console.log???
   console.log("string") 
 };
 
-module.exports = function str(string) { //converts 
+module.exports = function str(string) { //converts object to string
   return string.toString()
+};
+
+module.exports = function int(string) { //converts string to int
+  return parseInt(string)
 };
