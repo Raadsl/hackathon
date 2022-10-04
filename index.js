4a9b5381106f1d101cca384674023213d48bc78e
Jorik <83394320+Raadsl@users.noreply.github.com>
2022-10-04 16:58:28 +0200

    Update index.js
    added sleep()

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ module.exports = function log(string) { //shorter console.log???
   console.log("string") 
 };
 
+module.exports = function print(string) { //alternative console.log
+  console.log("string") 
+};
+
 module.exports = function str(string) { //converts object to string
   return string.toString()
 };
@@ -9,3 +13,16 @@ module.exports = function str(string) { //converts object to string
 module.exports = function int(string) { //converts string to int
   return parseInt(string)
 };
+
+module.exports = function print(string) { //converts string to int
+  console.log("string") 
+};
+
+module.exports = function sleep(time) { //sleep and continue after that. Usage: sleep(miliseconds) 
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > time){
+      break;
+    }
+  }
+}
