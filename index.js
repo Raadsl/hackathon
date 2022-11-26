b4835483037e448d99edb5027fc3decec8f1978f
Jorik <83394320+Raadsl@users.noreply.github.com>
2022-11-26 13:06:47 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,65 @@
-function log(string) {console.log(string)}; //shorter console.log???
+//Made by Raadsel aka Jorik.
+function log(object) {console.log(object)}; //shorter console.log???
 
-function print(string) {console.log(string)}; //alternative console.log
+function print(object) {console.log(object)}; //alternative console.log
 
-function str(string) {return string.toString()}; //converts object to string
+function str(object) {return object.toString()}; //converts object to string
 
-function int(string) {return parseInt(string)}; //converts string to int
+function int(object) {return parseInt(object)}; //converts object to int
 
+function float(object) {return parseFloat(object)}; //converts object to float
 
-function sleep(time) { //sleep and continue after that. Usage: sleep(miliseconds) 
+function ev(string) {eval(string)} //shorter eval()
+
+function sleep(miliseconds) { //sleep and continue after that. Usage: sleep(miliseconds) 
   var start = new Date().getTime();
   for (var i = 0; i < 1e7; i++) {
-    if ((new Date().getTime() - start) > time){
+    if ((new Date().getTime() - start) > miliseconds){
       break;
     }
   }
 }
 
-function random(min=0, max=100) {return Math.floor(Math.random() * (max - min + 1)) + min;} //random number between min and max
+function clear() {console.clear()} // clears console
 
+function random(min=0, max=100) {return Math.floor(Math.random() * (max - min + 1)) + min;} //random number between min and max\
+
+function randomColor() {return '#' + Math.floor(Math.random() * 16777215).toString(16);} //random color
+
+function randomColorRGB() {return 'rgb(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) + ')';} //random color rgb
+
+function randomColorRGBA() {return 'rgba(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 100) + ')';} //random color rgba
+
+function round(number, decimals=0) {return Number(Math.round(number+'e'+decimals)+'e-'+decimals)} //rounds number to decimals
+
+function getenv(envir) {
+  return process.env[envir];
+}
+
+function setenv(envir, value) {
+  process.env[envir] = value;
+}
+
+function allenvs() {
+  return process.env;
+}
 
 //export functions for npm
 module.exports = {
     log, //alternative console.log
     print, //alternative console.log
+    clear, //shorter console.clear. Clears console
     str, //converts object to string
-    int, //converts string to int
+    int, //converts object to int
+    float, //converts object to float
     sleep, //sleep and continue after that. Usage: sleep(miliseconds)
-    random //random number between min and max
+    random, //random number between min and max
+    randomColor, //random color
+    randomColorRGB, //random color rgb
+    randomColorRGBA, //random color rgba
+    round, //rounds number to decimals
+    ev, // shorter eval
+    getenv, //get environment variable ; alternative to process.env[env]
+    setenv, //set environment variable ; alternative to process.env[env] = value
+    allenvs //get all environment variables
 }
